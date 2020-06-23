a006620bb151eb0a490c638aa2f91a927e608e2f
Glenn Ruben Bakke <glennbakke@gmail.com>
2020-06-23 23:14:20 +0200

    nrf/drivers/lte: Add timer support for BSD library.

--- ports/nrf/drivers/lte/bsd_os.c
+++ ports/nrf/drivers/lte/bsd_os.c
@@ -6,6 +6,7 @@
 #include <nrf.h>
 #include "nrf_gpio.h"
 #include "errno.h"
+#include "py/mphal.h"
 
 #define BSD_OS_TRACE_ENABLED 0
 
@@ -30,6 +31,13 @@
 static nrfx_uarte_t m_uarte_instance = NRFX_UARTE_INSTANCE(BSD_OS_TRACE_UART_INSTANCE);
 
 #endif // BSD_OS_TRACE_ENABLED
+
+typedef struct {
+    uint32_t  context;
+    mp_uint_t prev_time;
+} bsd_os_timer_t;
+
+static bsd_os_timer_t timers[8];
 
 void read_task_create(void)
 {
@@ -70,9 +78,61 @@ void bsd_os_init(void) {
 #endif
 }
 
+int32_t locate_timer(uint32_t context) {
+    for (int i = 0; i < 8; i++)
+    {
+        if (timers[i].context == context)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int32_t locate_free(void) {
+    for (int i = 0; i < 8; i++)
+    {
+        if (timers[i].context == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int32_t bsd_os_timedwait(uint32_t context, int32_t * timeout)
 {
-    // TODO: Implement timers.
+    if (*timeout == BSD_OS_FOREVER) {
+        return 0;
+    }
+
+    if (*timeout == 0) {
+        return NRF_ETIMEDOUT;
+    }
+
+    mp_uint_t tick = mp_hal_ticks_ms();
+    // Locate timer or free spot.
+    int32_t timer = locate_timer(context);
+    if (timer == -1) {
+        timer = locate_free();
+
+        if (timer == -1) {
+            return NRF_ENOMEM;
+        }
+
+        timers[timer].context = context;
+        timers[timer].prev_time = tick;
+        // First time, only allocate.
+        return 0;
+    }
+    mp_uint_t diff = tick - timers[timer].prev_time;
+
+    if (diff > *timeout) {
+
+        timers[timer].context = 0;
+        timers[timer].prev_time = 0;
+        return NRF_ETIMEDOUT;
+    }
     return 0;
 }
 
